2e1d18e2ff1315180808580bd835e19ea6bdc637
Wanh3ll <wancleiton.cantanhede@gmail.com>
2025-10-18 02:19:38 -0300

    v4.1

--- mainProgram.py
+++ mainProgram.py
@@ -5,7 +5,7 @@ import random
 c = 0
 l = random.randint(10, 16)
 pyautogui.FAILSAFE = True
-pyautogui.PAUSE = random.randint(4, 10)/3
+pyautogui.PAUSE = random.randint(7, 16)/3
 time.sleep(10)
 
 from search import phrases
@@ -15,7 +15,7 @@ while c < l:
     pyautogui.press("backspace", presses=random.randint(3, 7))     
     pyautogui.write(random.choice(list(phrases)), interval=(random.randint(1,3)/8))
     pyautogui.press("enter")
-    time.sleep(random.randint(20, 30)/3)
+    time.sleep(random.randint(34, 50)/3)
     c += 1
 pyautogui.alert(f"**** COMPLETE COUNTER CICLE ****\nThe limit was {l}")
 
