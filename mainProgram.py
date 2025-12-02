723dda2f6de9f9202788767f90f6ab97d13116ee
wanh3ll <wancleiton.cantanhede@gmail.com>
2025-12-02 02:15:27 -0300

    v4.3.2

--- mainProgram.py
+++ mainProgram.py
@@ -5,7 +5,7 @@ import time
 import random
 
 c = 0
-l = random.randint(33, 46)
+l = random.randint(66, 86) # apenas para o dobro de pontos 
 pyautogui.FAILSAFE = True
 pyautogui.PAUSE = random.randint(7, 16)/3
 time.sleep(10)
@@ -17,6 +17,6 @@ while c < l:
     pyautogui.press("backspace", presses=random.randint(3, 7))     
     pyautogui.write(random.choice(list(phrases)), interval=(random.randint(1,3)/8))
     pyautogui.press("enter")
-    time.sleep(random.randint(40, 58)/3)
+    time.sleep(random.randint(35, 41)/3)
     c += 1
 pyautogui.alert(f"**** COMPLETE COUNTER CICLE ****\nThe limit was {l}")
