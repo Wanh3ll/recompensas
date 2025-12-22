3b6e19027e46d9a5ce34f7734283e2b95a2473ff
wanh3ll <wancleiton.cantanhede@gmail.com>
2025-12-22 20:43:29 -0300

    v5.1.1

--- mainProgram.py
+++ mainProgram.py
@@ -5,7 +5,7 @@ import time
 import random
 
 c = 0
-l = random.randint(65, 75) # reduzir 30 após terminar o dobro de pontos
+l = random.randint(62, 69) # reduzir 30 após terminar o dobro de pontos
 pyautogui.FAILSAFE = True
 pyautogui.PAUSE = random.randint(4, 7)/5
 time.sleep(10)
