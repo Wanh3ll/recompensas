2ec7fd7a31cebcae62a16394a5987a6e50611b77
wanh3ll <wancleiton.cantanhede@gmail.com>
2026-01-11 15:58:35 -0300

    v6.2.3

--- mainProgram.py
+++ mainProgram.py
@@ -7,22 +7,22 @@ import random
 c = 0
 l = random.randint(32, 39)
 pyautogui.FAILSAFE = True
-pyautogui.PAUSE = random.randint(4, 7)/11
+pyautogui.PAUSE = random.randint(4, 7)/11.3
 time.sleep(10)
 
 from search import phrases
 while c < l:
     pyautogui.hotkey("alt", "home")
     pyautogui.moveTo(x=(random.randint(1798, 1868)), 
-    y=(random.randint(333, 705)), duration=(random.randint(3,6)/5))
+    y=(random.randint(333, 705)), duration=(random.randint(3,6)/5.2))
     pyautogui.click()
     pyautogui.press("/")
     pyautogui.hotkey("ctrl", "a")
     pyautogui.press("backspace", presses=random.randint(3, 7))     
-    pyautogui.write(random.choice(list(phrases)), interval=(random.randint(2,4)/23))
+    pyautogui.write(random.choice(list(phrases)), interval=(random.randint(2,4)/22.7))
     pyautogui.press("enter")
-    time.sleep(random.randint(27, 37)/7)
+    time.sleep(random.randint(27, 37)/6.4)
     c += 1
-    time.sleep(random.randint(8, 11)/6)
+    time.sleep(random.randint(8, 11)/6.2)
         
 pyautogui.alert(f"**** COMPLETE COUNTER CICLE ****\nThe limit was {l}")
--- mouse_posicao.py
+++ mouse_posicao.py
@@ -7,6 +7,6 @@ time.sleep(5)
 print(pyautogui.position())
 
 while c < 20:
-    pyautogui.moveTo(x=(random.randint(1798, 1868)), 
-    y=(random.randint(333, 705)), duration=(random.randint(3,6)/5))
+    pyautogui.moveTo(x=(random.randint(980, 1060)), 
+    y=(random.randint(76, 115)), duration=(random.randint(3,6)/2.5))
     c += 1
