249fcb4eedaacfeb9c59ea2d38d0905810e43988
wanh3ll <wancleiton.cantanhede@gmail.com>
2026-04-09 18:47:07 -0300

    v9.1

--- Recompensas.py
+++ Recompensas.py
@@ -1,11 +1,20 @@
 from tkinter import Tk, Label, Button
+import pyautogui
 import edge
 import blueStacks
 
+
+res = (pyautogui.size())
+if res <= (1920, 1080):
+    resS = 350, 200
+    print(f"Resolução: {res[0]}x{res[1]}")
+elif res > (1920, 1080):
+    resS = 600, 350
+    print(f"Resolução: {res[0]}x{res[1]}")
 window = Tk()
 window.title("Recompensas")
-window.geometry("600x350")
-labelInitial = Label(window, text = "Selecione o programa desejado\n")
+window.geometry(f"{resS[0]}x{resS[1]}")
+labelInitial = Label(window, text = "\nSelecione o programa desejado\n", font = ("Arial", 12))
 labelInitial.pack() 
 
 def button1():  
@@ -13,7 +22,7 @@ def button1():
     edge.edgeMain()
     print("Script Edge Finalizado")
 
-button1 = Button(window, text = "EDGE", command = button1)
+button1 = Button(window, text = " EDGE ", command = button1)
 button1.pack()
 
 def button2():
@@ -21,7 +30,7 @@ def button2():
     blueStacks.blueMain()
     print("Script Blue Stacks finalizado")
 
-button2 = Button(window, text = "BLUE STACKS", command = button2)
+button2 = Button(window, text = " BLUE STACKS ", command = button2)
 button2.pack()
 
 window.mainloop() 
--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 from cx_Freeze import setup, Executable
 import sys
 
-# Lista de arquivos para incluir: ("origem", "destino_na_pasta_final")
 files = ["edge.wan", "blueStacks.wan"] 
 
 build_exe_options = {
     "include_files": files,
-    # Outras opções podem ser adicionadas aqui conforme necessário
 }
 executables = [
-    Executable("Recompensas.py", base=None, icon="icone.wan") # Substitua pelo nome do seu script
+    Executable("Recompensas.py", base=None, icon="icone.wan")
 ]
 setup(
     name="Recompensas",
