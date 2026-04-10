d65443b663155f1f262536fd0685b0d1bc34fe3e
wanh3ll <wancleiton.cantanhede@gmail.com>
2026-04-09 22:38:15 -0300

    v9.1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ build_exe_options = {
     "include_files": files,
 }
 executables = [
-    Executable("Recompensas.py", base=None, icon="icone.wan")
+    Executable("Recompensas.py", base=None, icon="icon.wan")
 ]
 setup(
     name="Recompensas",
