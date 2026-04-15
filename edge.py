b484eb68f78761a4a8fed51572c9201cf15e47df
wanh3ll <wancleiton.cantanhede@gmail.com>
2026-04-14 21:16:04 -0300

    v9.1.5

--- edge.py
+++ edge.py
@@ -18,8 +18,7 @@ def edgeMain():
         pyautogui.press("enter")    
         time.sleep(random.randint(27, 37)/6.4)
         c += 1
-        time.sleep(random.randint(8, 11)/6.2)             
-        #pyautogui.hotkey("ctrl", "w")
+        time.sleep(random.randint(8, 11)/6.2)
         pyautogui.hotkey("alt", "home")
         time.sleep(random.randint(8, 11)/6.2)   
     pyautogui.alert(f"**** COMPLETE COUNTER CICLE ****\nThe limit was {l}")
